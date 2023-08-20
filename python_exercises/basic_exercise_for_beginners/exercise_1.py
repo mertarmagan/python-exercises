0dbad4ba303d8b578933a97413c6a1b0806553bc
mertarmagan <mertarmagan@windowslive.com>
2023-08-21 00:47:55 +0300

    feat: solve exercise 1

--- python_exercises/basic_exercise_for_beginners/exercise_1.py
+++ python_exercises/basic_exercise_for_beginners/exercise_1.py
@@ -1,3 +1,31 @@
 # -*- coding: utf-8 -*-
-def solution():
-    pass
+"""Exercise 1: Calculate the multiplication and sum of two numbers
+
+Given two integer numbers return their product only if the product is equal to
+or lower than 1000, else return their sum.
+
+Given 1:
+number1 = 20
+number2 = 30
+
+Expected Output:
+The result is 600
+
+Given 2:
+number1 = 40
+number2 = 30
+
+Expected Output:
+The result is 70
+"""
+
+
+def solution(x=None, y=None):
+    # if x, y not given get user input instead
+    num1 = x if x else int(input("number1 = "))
+    num2 = y if y else int(input("number2 = "))
+
+    if num1 * num2 <= 1000:
+        return num1 * num2
+    else:
+        return num1 + num2
--- python_exercises/tests/basic_exercise_for_beginners/test_exercise_1.py
+++ python_exercises/tests/basic_exercise_for_beginners/test_exercise_1.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
-def test_solution():
-    pass
+from python_exercises.basic_exercise_for_beginners.exercise_1 import solution
+
+
+def test_solution_sum():
+    assert solution(10, 10) == 100
+
+
+def test_solution_product():
+    assert solution(200, 10) == 210
